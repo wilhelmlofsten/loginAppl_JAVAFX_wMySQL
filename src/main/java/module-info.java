fe7fc6e6a77ddd9f1f69ad1b5e41daaf18b4eaa6
wilhelm <willo509@student.liu.se>
2022-12-26 18:00:32 +0100

    Setup, default javaFX program works

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.loginappl {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.loginappl to javafx.fxml;
+    exports com.example.loginappl;
+}
