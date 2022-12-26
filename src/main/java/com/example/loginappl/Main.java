41e15b19d88faca554ff23c94b33f213c9c40877
wilhelm <willo509@student.liu.se>
2022-12-26 21:57:15 +0100

    change resolution

--- src/main/java/com/example/loginappl/Main.java
+++ src/main/java/com/example/loginappl/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 640, 440);
+        stage.setTitle("Welcome!");
         stage.setScene(scene);
         stage.show();
     }
